05c5cd815419f9e79f099e07ac779140462b9ab3
Rama1520 <ignacio_nico09@hotmail.com>
2026-05-15 20:20:06 -0300

    fix2

--- carta.js
+++ carta.js
@@ -11,29 +11,65 @@ class Carta {
     }
 
     static createFromJsonString(json) {
-       
         let obj = json; 
         return new Carta(obj.code, obj.value, obj.suit, obj.imagen);
     }
 
     createHtmlElement() {
         let divCarta = document.createElement("div");
-        divCarta.className = "tarjeta"; 
+        divCarta.className = "tarjeta";
 
         let img = document.createElement("img");
         img.src = this.imagen;
         img.style.width = "100%"; 
+        img.style.cursor = "pointer";
+        img.addEventListener("click", () => {
+            window.open(this.imagen, "_blank");
+        });
 
         let titulo = document.createElement("h3");
         titulo.innerText = this.value + " of " + this.suit;
 
         let parrafoCodigo = document.createElement("p");
-        parrafoCodigo.innerText = "Codigo: " + this.code;
+        parrafoCodigo.innerText = "Código: " + this.code;
+
+        let btnGuardar = document.createElement("button");
+        btnGuardar.innerText = "Guardar";
+        btnGuardar.className = "btn btn-outline-primary"; 
+        btnGuardar.addEventListener("click", () => {
+            Carta.guardarCarta(this);
+        });
 
         divCarta.appendChild(img);
         divCarta.appendChild(titulo);
         divCarta.appendChild(parrafoCodigo);
+        divCarta.appendChild(btnGuardar);
 
         return divCarta;
+    }
+
+    static guardarCarta(carta) {
+        let guardadas = localStorage.getItem("cartasGuardadas");
+        if(guardadas == null) {
+            guardadas = [];
+        } else {
+            guardadas = JSON.parse(guardadas);
+        }
+
+        let repetida = false;
+        for(let i = 0; i < guardadas.length; i++) {
+            if(guardadas[i].code === carta.code) {
+                repetida = true;
+            }
+        }
+
+        if(!repetida) {
+            guardadas.push(carta);
+            
+            localStorage.setItem("cartasGuardadas", guardadas);
+            alert("Carta guardada correctamente!");
+        } else {
+            alert("Esta carta ya la habías guardado.");
+        }
     }
 }
--- main.js
+++ main.js
@@ -1,22 +1,47 @@
+let pagina = 1;
+
 document.addEventListener("DOMContentLoaded", () => {
     cargarCartas();
+
+    let btnSig = document.getElementById("siguiente");
+    let btnAnt = document.getElementById("anterior");
+
+    btnSig.addEventListener("click", paginaSiguiente);
+    btnAnt.addEventListener("click", paginaAnterior);
 });
 
 async function cargarCartas() {
     try {
         let respuesta = await fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=6");
         let datos = await respuesta.json();
-
         let contenedor = document.getElementById("cartas");
+        contenedor.innerHTML = ""; 
 
         for (let i = 0; i < datos.cards.length; i++) {
             let c = datos.cards[i];
             let nuevaCarta = new Carta(c.code, c.value, c.suit, c.image);
-            
             contenedor.appendChild(nuevaCarta.createHtmlElement());
+        }
+
+        if(pagina === 1) {
+            document.getElementById("anterior").disabled = true;
+        } else {
+            document.getElementById("anterior").disabled = false;
         }
 
     } catch (error) {
         console.log("Hubo un error al traer las cartas: " + error);
+    }
+}
+
+function paginaSiguiente() {
+    pagina++;
+    cargarCartas();
+}
+
+function paginaAnterior() {
+    if (pagina > 1) {
+        pagina--;
+        cargarCartas();
     }
 }
